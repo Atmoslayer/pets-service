539999abe3f7ce54860aa439d520ee390e7c7077
Atmoslayer <ddsomdim@gmail.com>
2023-12-11 00:32:06 +0300

    Create API views

--- pets/views.py
+++ pets/views.py
@@ -1,17 +1,60 @@
-from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import viewsets, permissions
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication, \
+    BasicAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
 
-from .serializers import PetSerializer
+from .serializers import PetSerializer, PetSerializerMini, UserSerializer
 
 
 class PetViewSet(viewsets.ModelViewSet):
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    authentication_classes = [SessionAuthentication, BasicAuthentication, TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = PetSerializer
+
+    def list(self, request, *args, **kwargs):
+        user = self.request.user
+        if user.is_authenticated:
+            user_pets = user.pets.all()
+        serializer = PetSerializerMini(user_pets, many=True)
+        return Response(serializer.data)
 
     def get_queryset(self):
         user = self.request.user
-        user_pets = user.pets.all()
+        if user.is_authenticated:
+            user_pets = user.pets.all()
+            return user_pets
+        else:
+            return None
 
-        return user_pets
 
+class CreateUserAPpiView(CreateAPIView):
+    model = User
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = UserSerializer
+
+    @classmethod
+    def get_extra_actions(cls):
+        return []
+
+
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'id': user.id,
+            'token': token.key,
+            'username': user.username,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+            'email': user.email,
+        })
