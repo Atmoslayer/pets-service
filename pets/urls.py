f0b242a786487445faca25ae8758398e6c503b90
Atmoslayer <ddsomdim@gmail.com>
2023-12-11 00:33:25 +0300

    Add pets app urls

--- pets/urls.py
+++ pets/urls.py
@@ -3,7 +3,7 @@ from rest_framework import routers
 from pets.views import PetViewSet
 
 router = routers.DefaultRouter()
-router.register('api/pets', PetViewSet, 'show_pets')
+router.register('pets', PetViewSet, basename='all_pets')
 
 urlpatterns = router.urls
 
