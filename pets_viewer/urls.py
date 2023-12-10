ecef2fb4c4020c3f23414a0098d86e4c493d2d0e
Atmoslayer <ddsomdim@gmail.com>
2023-12-11 00:30:23 +0300

    Add all main urls

--- pets_viewer/urls.py
+++ pets_viewer/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from pets.views import CustomAuthToken, CreateUserAPpiView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pets.urls'))
+    path('api/auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/registration/', CreateUserAPpiView.as_view()),
+    path('api-token-auth/', CustomAuthToken.as_view()),
+    path('api/', include('pets.urls')),
 ]
