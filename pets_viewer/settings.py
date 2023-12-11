b7675ebc47c6bca90c29edcc4e3aa4e3ed0fb606
Atmoslayer <ddsomdim@gmail.com>
2023-12-11 15:51:03 +0300

    Attach postgres

--- pets_viewer/settings.py
+++ pets_viewer/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'pets_viewer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': POSTGRES_DB,
+        'USER': POSTGRES_USER,
+        'PASSWORD': POSTGRES_PASSWORD,
+        'HOST': POSTGRES_HOST,
+        'PORT': POSTGRES_PORT,
     }
 }
 
